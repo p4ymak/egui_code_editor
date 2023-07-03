c6fc4269ddc80581123ea2435c63cd37e5b22e5f
Roman Chumak <p4ymak@yandex.ru>
2023-07-03 19:18:42 +0300

    rust true false

--- src/syntax/rust.rs
+++ src/syntax/rust.rs
@@ -9,17 +9,17 @@ impl Syntax {
             case_sensitive: true,
             comment: "//",
             keywords: HashSet::from([
-                "as", "break", "const", "continue", "crate", "else", "enum", "extern", "false",
-                "fn", "for", "if", "impl", "in", "let", "loop", "match", "mod", "move", "mut",
-                "pub", "ref", "return", "self", "struct", "super", "trait", "true", "type", "use",
-                "where", "while", "async", "await", "abstract", "become", "box", "do", "final",
-                "macro", "override", "priv", "typeof", "unsized", "virtual", "yield", "try",
+                "as", "break", "const", "continue", "crate", "else", "enum", "extern", "fn", "for",
+                "if", "impl", "in", "let", "loop", "match", "mod", "move", "mut", "pub", "ref",
+                "return", "self", "struct", "super", "trait", "type", "use", "where", "while",
+                "async", "await", "abstract", "become", "box", "do", "final", "macro", "override",
+                "priv", "typeof", "unsized", "virtual", "yield", "try",
             ]),
             types: HashSet::from([
                 "bool", "i8", "u8", "i16", "u16", "i32", "u32", "i64", "u64", "i128	", "u128",
                 "isize", "usize", "f32", "f64", "str", "String", "Vec",
             ]),
-            special: HashSet::from(["dyn", "Self", "static", "unsafe"]),
+            special: HashSet::from(["dyn", "Self", "static", "unsafe", "true", "false"]),
         }
     }
 }
